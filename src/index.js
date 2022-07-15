40c09f6a6e79ac8ad402d197bb4cbcfa5ffd89fb
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-15 13:52:41 +0300

    dwdw

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-let arr = [1, 5, 6];
-console.log(arr);
-console.log("hi");
-arr[3] = 8;
+function combine(...obj) {
+  obj.reduce((acc, value) => {
+    acc[value] = 
+  }, {})
+}
+  const objA = { a: 10, b: 20, c: 30 }
+  const objB = { a: 3, c: 6, d: 3 }
+
+combine(objA, objB)
