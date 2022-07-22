5c9d5a6b1735c30651ba620b22c346e9c23ca23f
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-22 16:13:22 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,54 +1,77 @@
-const ageTable = {
-  firstSonAge: 14,
-  secondSonAge: 9,
-  thirdSonAge: 8
-};
+const data1 = [
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1,
+  0,
+  1,
+  0,
+  1,
+  0
+];
+const data2 = [
+  1,
+  0,
+  1,
+  0,
+  1,
+  0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1
+];
 
-const diary1 = {
-  algebra: 6,
-  history: 7,
-  physics: 8,
-  geography: 9,
-  chemistry: 10
-};
-
-const diary2 = {
-  algebra: 8,
-  history: 7,
-  physics: 8,
-  geography: 9,
-  chemistry: 10
-};
-
-const diary3 = {
-  algebra: 6,
-  history: 5,
-  physics: 5,
-  geography: 9,
-  chemistry: 10
-};
-
-function whoseBicycle(diary1, diary2, diary3) {
-  let firstSonMark = 0;
-  let secondSonMark = 0;
-  let thirdSonMark = 0;
-
-  for (let key in diary1) {
-    firstSonMark += diary1[key];
+function dataReverse(data) {
+  let arr = [];
+  for (let i = 0; i < data.length; i += 7) {
+    arr.push(data.splice(i, 7));
   }
-
-  for (let key in diary2) {
-    secondSonMark += diary2[key];
-  }
-
-  for (let key in diary3) {
-    thirdSonMark += diary3[key];
-  }
-
-  if (firstSonMark === Math.max(firstSonMark, secondSonMark, thirdSonMark)) {
-  } else if (
-    secondSonMark === Math.max(firstSonMark, secondSonMark, thirdSonMark)
-  ) {
-  } else {
-  }
+  return arr;
 }
+console.log(dataReverse(data1));
