dff8e5cd844f58b4dc7dc9d054719179f82354b3
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-22 15:12:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -27,3 +27,28 @@ const diary3 = {
   geography: 9,
   chemistry: 10
 };
+
+function whoseBicycle(diary1, diary2, diary3) {
+  let firstSonMark = 0;
+  let secondSonMark = 0;
+  let thirdSonMark = 0;
+
+  for (let key in diary1) {
+    firstSonMark += diary1[key];
+  }
+
+  for (let key in diary2) {
+    secondSonMark += diary2[key];
+  }
+
+  for (let key in diary3) {
+    thirdSonMark += diary3[key];
+  }
+
+  if (firstSonMark === Math.max(firstSonMark, secondSonMark, thirdSonMark)) {
+  } else if (
+    secondSonMark === Math.max(firstSonMark, secondSonMark, thirdSonMark)
+  ) {
+  } else {
+  }
+}
